d4bfdc42b932e121e5dfc979780c2a6276c56b02
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 19:08:30 +0100

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,6 +1,7 @@
-import React from "@interfaces/index.tsx";
+import React from "react";
+import { ButtonProps } from "../interfaces";
 
-const Button = ({ title, styles }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button className={`px-4 py-2 font-semibold text-white bg-blue-600 ${styles}`}>
       {title}
